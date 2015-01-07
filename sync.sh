48ce181697d37d075438e58bff968df1f5580e75
Madhurendra <themadhurendra@gmail.com>
2015-01-07 07:24:39 +0530

    More option, for feasible transfer.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
-rsync -rv --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/tikaj.com/include
-rsync -rv --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/api.maarpeet.com/include
-rsync -rv --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/madhurendra.com/include
-rsync -rv --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/ika.pw/include
-rsync -rv --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/ij.com/include
-rsync -rv --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/upadi-vet.com/include
+rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/tikaj.com/include
+rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/api.tgm.pw/include
+rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/madhurendra.com/include
+rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/ika.pw/include
+rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/ij.com/include
+rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/upadi-vet.com/include
