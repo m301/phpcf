88631a9c3573c60c8c3830fbab7a7ed305b6d3d0
Madhurendra <themadhurendra@gmail.com>
2015-01-07 07:24:04 +0530

    Get & Post content now accept curl options from user.

--- class.DataHandler.php
+++ class.DataHandler.php
@@ -1,39 +1,49 @@
 <?PHP
+
 /**
  * DataHandler
- * 
+ *
  * @package default
  * @author Madhurendra Sachan
  * @copyright 2014 Madhurendra Sachan
- * @version v1.01
+ * @version v1.02
  */
- 
 class DataHandler
 {
     /**
      * getContent()
-     * 
+     *
      * fetches a url & returns the data/html
-     * 
+     *
      * @param mixed $url
+     * @param array $options
      * @return string
+     * @throws Exception
      */
-    public static function getContent($url)
+    public static function getContent($url, $options = array())
     {
+        //echo $url;
         $ch = curl_init();
-        curl_setopt_array($ch,array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HEADER => false,
-           // CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0",
-            CURLOPT_AUTOREFERER => true,
-            CURLOPT_CONNECTTIMEOUT => 120,
-            CURLOPT_TIMEOUT => 120,
-            CURLOPT_MAXREDIRS => 10));
 
-        $result= curl_exec($ch);
+        $options = array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER => false,
+                // CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0",
+                CURLOPT_AUTOREFERER => true,
+                CURLOPT_CONNECTTIMEOUT => 120,
+                CURLOPT_TIMEOUT => 120,
+                CURLOPT_MAXREDIRS => 10) + $options;
+
+        curl_setopt_array($ch, $options);
+        $result = curl_exec($ch);
+
+        if ($c_error_no = curl_errno($ch))
+            throw new Exception("[$c_error_no]" . curl_error($ch));
+
+
         curl_close($ch);
 
         return $result;
@@ -41,21 +51,23 @@ class DataHandler
 
     /**
      * postContent()
-     * 
+     *
      * makes a post request on given url with given data & returns the response
-     * 
+     *
      * @param mixed $url
-     * @param mixed $data,data to attach in post request
+     * @param mixed $data ,data to attach in post request
+     * @param array $options
      * @return string
+     * @throws Exception
      */
-    public static function postContent($url, $data)
+    public static function postContent($url, $data, $options = array())
     {
         $ch = curl_init();
-        curl_setopt_array($ch,array(
+        $options = array(
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HEADER => false,
-          //  CURLOPT_FOLLOWLOCATION => true,
+            //  CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_ENCODING => "",
             CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0",
             CURLOPT_AUTOREFERER => true,
@@ -63,13 +75,20 @@ class DataHandler
             CURLOPT_TIMEOUT => 120,
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => $data));
-        $result= curl_exec($ch);
+            CURLOPT_POSTFIELDS => $data)+$options;
+        curl_setopt_array($ch, $options );
+        $result = curl_exec($ch);
+
+        if ($c_error_no = curl_errno($ch))
+            throw new Exception("[$c_error_no]" . curl_error($ch));
+
+
         curl_close($ch);
         return $result;
     }
 
-    public static function downloadFile($url,$filepath){
+    public static function downloadFile($url, $filepath)
+    {
         file_put_contents($filepath, fopen($url, 'r'));
     }
 }
