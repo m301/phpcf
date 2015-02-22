4524f2fd6b915143e8ea853b0741387fa741942d
Madhurendra Sachan <madhurendra@tikaj.com>
2015-02-22 12:12:23 +0530

    Added zipDir & deleteDir

--- functions.basic.php
+++ functions.basic.php
@@ -52,3 +52,88 @@ function isValidUsername($str)
 {
     return preg_match('/^[a-zA-Z0-9_]+$/', $str);
 }
+
+
+function zipDir($source, $destination, $include_dir = false)
+{
+
+    if (!extension_loaded('zip') || !file_exists($source)) {
+        return false;
+    }
+
+    if (file_exists($destination)) {
+        unlink ($destination);
+    }
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+        return false;
+    }
+    $source = str_replace('\\', '/', realpath($source));
+
+    if (is_dir($source) === true)
+    {
+
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        if ($include_dir) {
+
+            $arr = explode("/",$source);
+            $maindir = $arr[count($arr)- 1];
+
+            $source = "";
+            for ($i=0; $i < count($arr) - 1; $i++) { 
+                $source .= '/' . $arr[$i];
+            }
+
+            $source = substr($source, 1);
+
+            $zip->addEmptyDir($maindir);
+
+        }
+
+        foreach ($files as $file)
+        {
+            $file = str_replace('\\', '/', $file);
+
+            // Ignore "." and ".." folders
+            if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                continue;
+
+            $file = realpath($file);
+
+            if (is_dir($file) === true)
+            {
+                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            }
+            else if (is_file($file) === true)
+            {
+                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+        }
+    }
+    else if (is_file($source) === true)
+    {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+    
+    return $zip->close();
+}
+
+function deleteDir($dirPath) {
+    if (! is_dir($dirPath)) {
+        throw new InvalidArgumentException("$dirPath must be a directory");
+    }
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            self::deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
+}
