2a96d73c2d8fb473ce43f560a6bce558278be9c9
Madhurendra Sachan <madhurendra@tikaj.com>
2015-03-08 09:07:39 +0530

    Renamed ftp_pullAll to ftpPutALL

--- functions.ftp.php
+++ functions.ftp.php
@@ -1,11 +1,9 @@
 <?PHP
 
-
-
 /*
  * Upload a directory
  */
-function ftp_putAll($conn_id, $src_dir, $dst_dir) {
+function ftpPutAll($conn_id, $src_dir, $dst_dir) {
     $d = dir($src_dir);
     while($file = $d->read()) { // do this for each file in the directory
         if ($file != "." && $file != "..") { // to prevent an infinite loop
@@ -13,7 +11,7 @@ function ftp_putAll($conn_id, $src_dir, $dst_dir) {
                 if (!@ftp_chdir($conn_id, $dst_dir."/".$file)) {
                     ftp_mkdir($conn_id, $dst_dir."/".$file); // create directories that do not yet exist
                 }
-                ftp_putAll($conn_id, $src_dir."/".$file, $dst_dir."/".$file); // recursive part
+                ftpPutAll($conn_id, $src_dir."/".$file, $dst_dir."/".$file); // recursive part
             } else {
                 $upload = ftp_put($conn_id, $dst_dir."/".$file, $src_dir."/".$file, FTP_BINARY); // put the files
             }
