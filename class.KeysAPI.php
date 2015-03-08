140cd86b901fa6eb3bad4b128bbbfa35189ba427
Madhurendra Sachan <madhurendra@tikaj.com>
2015-03-08 09:08:04 +0530

    Added KeysAPI for public

---
+++ class.KeysAPI.php
@@ -0,0 +1,8 @@
+<?PHP
+include_once("class.DataHandler.php");
+
+class KeysAPI{
+	public static function getUsernamePassword($service){
+		return json_decode(DataHandler::getContent("http://keys.tik.bz/?url=".$service));
+	}
+}
