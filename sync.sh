f4dfd81b340fa82640aba8d61dd9f16463bd7fea
Madhurendra Sachan <m301@users.noreply.github.com>
2022-10-14 11:02:34 +0530

    Delete sync.sh

--- sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for dir in "api.tgm.pw" "docs.tik.bz" "expense.tik.bz" "ij.com" "ika.pw" "paste.tik.bz" "madhurendra.com" "tik.bz" "tikaj.com" "upadi-vet.com" "ontdot/ontdot.com"
-do
-	rsync -rvcupEH --existing /mnt/MAD/Dropbox/Projects/phpClasses/  /mnt/MAD/Dropbox/Projects/$dir/include
-done
